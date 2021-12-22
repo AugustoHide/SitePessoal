ed49e07d41196658ce5374bc9967498059280df7
AugustoHide <augustohide@gmail.com>
2021-12-22 14:27:04 -0400

    resolver bug de redirecionamento https

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'html');
 
-
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
+        res.redirect(`https://${req.hostname}${req.url}`); //Redireciona pra HTTPS
+    else //Se a requisição já é HTTPS
+        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+});
 
 
 
